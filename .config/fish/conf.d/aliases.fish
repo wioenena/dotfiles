25aedf76c1799492807e870dae54d8762cb7209a
wioenena <wioenena@gmail.com>
2026-02-11 20:40:27 +0300

    .

---
+++ .config/fish/conf.d/aliases.fish
@@ -0,0 +1,4 @@
+alias man batman
+alias ls "eza --all --long --icons always --group-directories-first"
+alias cd z
+alias zed zeditor
---
+++ .config/fish/conf.d/nix.fish
@@ -0,0 +1,3 @@
+set --global XDG_DATA_DIRS "$HOME/.nix-profile/share:$XDG_DATA_DIRS"
+
+fish_add_path "$HOME/.nix-profile/bin"
