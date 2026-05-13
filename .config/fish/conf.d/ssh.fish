c99b6ef4857184a97d6e86a2cf9d05ffd9f5988b
wioenena <wioenena@gmail.com>
2026-05-13 04:06:16 +0300

    chore: add ssh config and fish agent setup

---
+++ .config/fish/conf.d/ssh.fish
@@ -0,0 +1,7 @@
+set -gx SSH_ASKPASS "/usr/bin/ksshaskpass"
+set -gx SSH_ASKPASS_REQUIRE "prefer"
+
+
+if test -z "$SSH_AUTH_SOCK"; or not test -e "$SSH_AUTH_SOCK"
+    eval (ssh-agent -c) > /dev/null
+end
